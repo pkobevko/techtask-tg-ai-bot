d8cb5f88d5ef57ac01b58ee24382429b21acfc41
Petro Kobevko <kobevkopi@gmail.com>
2023-11-20 17:10:42 +0200

    Refactored code according to the checkstyle

--- src/main/java/com/tttgaibot/config/SwaggerConfig.java
+++ src/main/java/com/tttgaibot/config/SwaggerConfig.java
@@ -1,5 +1,9 @@
 package com.tttgaibot.config;
 
+import java.util.Collections;
+import java.util.List;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
@@ -11,15 +15,10 @@ import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spi.service.contexts.SecurityContext;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
-import java.util.Collections;
-import java.util.List;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
 
 @Configuration
 @EnableSwagger2
 public class SwaggerConfig {
-
     public ApiInfo apiInfo() {
         return new ApiInfoBuilder()
                 .title("TechTask Telegram AI Bot backend API")
